afe24341882b8a1be96150955997d6a0a7fb55bc
Mi-Mi-Mishka <99869800+Mi-Mi-Mishka@users.noreply.github.com>
2026-05-10 13:15:09 +0300

    добавил кнопку установки приложения

--- public/js/main.js
+++ public/js/main.js
@@ -117,4 +117,19 @@
     } else {
         init();
     }
+
+    let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    const installBtn = document.getElementById('installBtn');
+    if (installBtn) installBtn.style.display = 'block';
+    
+    installBtn?.addEventListener('click', () => {
+        installBtn.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then(() => { deferredPrompt = null; });
+    });
+});
 })();
